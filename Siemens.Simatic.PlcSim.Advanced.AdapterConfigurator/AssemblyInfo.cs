c2d055bfc2b5e775943dca61474e116ca7ea3b03
yanzixiang <whyzix@qq.com>
2018-11-11 14:53:18 +0800

    添加文档

---
+++ Siemens.Simatic.PlcSim.Advanced.AdapterConfigurator/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Siemens.Simatic.PlcSim.Advanced.AdapterConfigurator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Siemens AG")]
+[assembly: AssemblyProduct("Siemens.Simatic.PlcSim.Advanced.AdapterConfigurator")]
+[assembly: AssemblyCopyright("Copyright © Siemens AG 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("84094a02-6979-4b58-b1e1-c444003348b8")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Siemens.Simatic.PlcSim.Advanced.AdapterConfigurator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Siemens.Simatic.PlcSim.Advanced.AdapterConfigurator
+{
+  internal class Program
+  {
+    private static IConfigureAdaptersNET ConfigureCommunicationAdapters;
+
+    private static int Main(string[] args)
+    {
+      Debugger.Launch();
+      ConfigureCommunicationAdapters = new IConfigureAdaptersNETImpl();
+      int num = -2;
+      if (args.Length == 2)
+      {
+        try
+        {
+          string str = args[0];
+          string AdapterName = args[1];
+          if (str.Length > 50 || AdapterName.Length > 200)
+            return num;
+          if (!(str == "EnableVirtualSwitch"))
+          {
+            num = str == "DisableVirtualSwitch" ? (ConfigureCommunicationAdapters.ConfigureVirtualSwitch(AdapterName, false) ? 0 : -5) : (str == "ResetVirtualSwitchAndIPv6" ? (ConfigureCommunicationAdapters.ResetNetworkSettings() ? 0 : -5) : -4);
+          }
+          else
+          {
+            ConfigureCommunicationAdapters.ResetNetworkSettings();
+            num = ConfigureCommunicationAdapters.ConfigureVirtualSwitch(AdapterName, true) ? 0 : -5;
+          }
+        }
+        catch (Exception ex)
+        {
+          num = -3;
+        }
+      }
+      return num;
+    }
+  }
+}
